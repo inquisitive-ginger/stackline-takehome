b1b7aef201b987ba7272ecf26fa3ae53c5438f36
Zach Pritchett <pritchett.za@gmail.com>
2023-12-13 22:22:57 -0800

    🙈

--- src/products/ProductSalesChart.tsx
+++ src/products/ProductSalesChart.tsx
@@ -9,6 +9,7 @@ import {
   Legend,
 } from "chart.js";
 import { Line } from "react-chartjs-2";
+import { useWindowSize } from "@uidotdev/usehooks";
 
 import { ProductSales } from "./types";
 import prepareChartData, {
@@ -29,13 +30,20 @@ type Props = {
   data: ProductSales[];
 };
 
+const M = 0.01844273;
+const B = 41.5013;
+
 const ProductSalesChart = ({ data }: Props) => {
   const chartData = prepareChartData(data);
+  const size = useWindowSize();
+
+  // 🙈 attempt to make the chart somewhat responsive
+  const vw = Math.floor(M * size.width! + B);
 
   return (
     <div className="shadow-md p-4 min-h-[600px] bg-white flex flex-col w-full">
       <h1 className="text-lg text-gray-500">Retail Sales</h1>
-      <div className="relative lg:w-[64vw] xl:w-[74vw] h-full">
+      <div className={`relative w-[${vw}vw] h-full`}>
         <Line options={salesChartOptions} data={chartData} />
       </div>
     </div>
